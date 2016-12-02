4837479a53e2104512a471b015c5e6595faf27ee
hard-max <maxvoronin.dev@gmail.com>
2016-12-02 17:57:07 +0200

    setup yii2

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=find_pets',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
